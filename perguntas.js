d1ad7ab28736d0014f648180bfeca9524022c706
Nicollymarques16 <159157738+Nicollymarques16@users.noreply.github.com>
2024-10-08 12:22:21 -0300

    08/10

---
+++ perguntas.js
@@ -0,0 +1,37 @@
+criaCartao(
+    'Programação',
+    'O que é JavaSript?',
+    'O javaSript é uma linguagem de programação.'
+)
+
+criaCartao(
+ 'Geografia',
+ 'Qual é a capital da França?',
+ 'A capital da França é Paris'
+
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de códigos que executa uma função.'
+)
+
+criaCartao(
+    'Matemática',
+    'Qual é o resultado de 6 x 8?',
+    'O resultado é 48'
+
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é a capital da Italia?',
+    'A capital é Roma.'
+)
+
+criaCartao(
+    'ciencia',
+    "O Sol é oque?",
+    'Uma estrela.'
+)
